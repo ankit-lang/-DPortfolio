9f5a2adf1ba8b9831da42cb57c2514abcb040c7e
Ankit <rankit2883@gmail.com>
2024-07-09 11:56:44 +0530

    modified

--- src/constants/index.js
+++ src/constants/index.js
@@ -66,34 +66,25 @@ const services = [
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
+ 
+  
   {
     name: "JavaScript",
     icon: javascript,
   },
-  {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript,
+  // },
+
   {
     name: "Redux Toolkit",
     icon: redux,
   },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
+  // {
+  //   name: "Tailwind CSS",
+  //   icon: tailwind,
+  // },
   {
     name: "Node JS",
     icon: nodejs,
@@ -103,21 +94,18 @@ const technologies = [
     icon: mongodb,
   },
   {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
     name: "docker",
-    icon: docker,
+    icon: docker ,
   },
+ 
+  
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+ 
+ 
+ 
 ];
 
 const experiences = [
